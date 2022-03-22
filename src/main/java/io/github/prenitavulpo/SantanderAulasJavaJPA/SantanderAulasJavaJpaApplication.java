a4421aff84a10efe5aa8ae2a6a052b661e499ee6
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-03-21 21:23:20 -0300

    add PessoaRepository and Command line app

--- src/main/java/io/github/prenitavulpo/SantanderAulasJavaJPA/SantanderAulasJavaJpaApplication.java
+++ src/main/java/io/github/prenitavulpo/SantanderAulasJavaJPA/SantanderAulasJavaJpaApplication.java
@@ -1,7 +1,9 @@
 package io.github.prenitavulpo.SantanderAulasJavaJPA;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SantanderAulasJavaJpaApplication {
@@ -10,4 +12,10 @@ public class SantanderAulasJavaJpaApplication {
 		SpringApplication.run(SantanderAulasJavaJpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(){
+		return args ->{
+			System.out.println("teste");
+		};
+	}
 }
